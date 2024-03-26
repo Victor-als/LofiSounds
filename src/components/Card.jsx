ab4e242cd02d0c2fd45dc7fd99c598e0103c69dd
unknown <victor.ccx814@gmail.com>
2024-03-26 12:15:50 -0300

    fix: input volume

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,14 +12,29 @@ export default function Card({ imageSrc, imageAlt, audioSrc}) {
   const [play, { stop }] = useSound(audioSrc, { volume: volume, loop: true });
 
 
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    isClicked ? stop() : play();
+  }
+
+  const handleSliderChange = (e) => {
+    setVolume(parseFloat(e.target.value));
+  }
+
+  const handleSliderClick = (e) => {
+    e.stopPropagation()
+  }
+
   return(
-    <div className={`w-[300px] h-[260px] hover:cursor-pointer hover:scale-105 ]
-    hover:shadow-[0_20px_50px_rgba(8,_122,_184,_0.7)] transition-all relative rounded-lg
-     ${isClicked ? "scale-105 shadow-[0_20px_50px_rgba(8,_122,_184,_0.7)]" : "shadow-none"}`}
-     onClick={() => { 
-      setIsClicked(!isClicked);
-      isClicked ? stop() : play();
-     }}
+    <div 
+    className={
+     `w-[300px] h-[260px] hover:cursor-pointer hover:scale-105 ]
+      hover:shadow-[0_20px_50px_rgba(8,_122,_184,_0.7)] transition-all 
+      relative rounded-lg
+     ${isClicked 
+      ? "scale-105 shadow-[0_20px_50px_rgba(8,_122,_184,_0.7)]" 
+      : "shadow-none"}`}
+     onClick={handleClick}
      >
  
       <audio autoPlay>
@@ -31,9 +46,14 @@ export default function Card({ imageSrc, imageAlt, audioSrc}) {
 
 
       <div className="absolute flex p-2 w-full">
-        <input type="range" max="1" min="0" step="0.01"
-        className="relative z-2 bottom-10 w-full" 
-        onChange={(e) => setVolume(e.target.value)}/>
+        <input 
+          type="range" 
+          max="1" 
+          min="0" 
+          step="0.01"
+          className="relative z-2 bottom-10 w-full" 
+          onClick={handleSliderClick}
+          onChange={handleSliderChange}/>
       </div>
     </div>
   );
